739f837a55a66a3f840328affd39e2f640775e3c
Júlio César T. dos Santos <36892197+j086795@users.noreply.github.com>
2019-04-16 15:45:56 -0300

    Add files via upload
    Teste finalizado para fazer a prova

---
+++ Teste_para_prova/Teste_para_prova/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Teste_para_prova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnConfirma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            resources.ApplyResources(this.lblUser, "lblUser");
+            this.lblUser.Name = "lblUser";
+            // 
+            // lblSenha
+            // 
+            resources.ApplyResources(this.lblSenha, "lblSenha");
+            this.lblSenha.Name = "lblSenha";
+            // 
+            // txtUser
+            // 
+            resources.ApplyResources(this.txtUser, "txtUser");
+            this.txtUser.Name = "txtUser";
+            // 
+            // txtSenha
+            // 
+            resources.ApplyResources(this.txtSenha, "txtSenha");
+            this.txtSenha.Name = "txtSenha";
+            // 
+            // btnConfirma
+            // 
+            resources.ApplyResources(this.btnConfirma, "btnConfirma");
+            this.btnConfirma.Name = "btnConfirma";
+            this.btnConfirma.UseVisualStyleBackColor = true;
+            this.btnConfirma.Click += new System.EventHandler(this.btnConfirma_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.btnConfirma);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnConfirma;
+    }
+}
+
---
+++ Teste_para_prova/Teste_para_prova/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Teste_para_prova
+{
+    public partial class Form1 : Form
+    {
+        Thread nt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+
+            if(txtSenha.Text == "1234")
+            {
+                this.Close();
+                nt = new Thread(novoForm);
+                nt.SetApartmentState(ApartmentState.STA);
+                nt.Start();
+
+            }
+            else if(txtSenha.Text == "")
+            {
+                MessageBox.Show("Informe a senha!","Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                 txtSenha.Focus();
+            } else
+            {
+                MessageBox.Show("Senha incorreta!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSenha.Clear();
+                txtSenha.Focus();
+            }
+        }
+
+        private void novoForm()
+        {
+            Application.Run(new Form2());
+        }
+    }
+}
