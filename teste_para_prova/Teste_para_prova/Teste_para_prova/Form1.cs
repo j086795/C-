64e9798bca2383cd13c1785f855a746dcac266ea
Júlio César <julio_splinter@outlook.com>
2019-04-15 22:41:53 -0300

    Teste para prova

---
+++ teste_para_prova/Teste_para_prova/Teste_para_prova/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Teste_para_prova
+{
+    public partial class Form1 : Form
+    {
+        Thread nt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+
+            if(txtSenha.Text == "1234")
+            {
+                this.Close();
+                nt = new Thread(novoForm);
+                nt.SetApartmentState(ApartmentState.STA);
+                nt.Start();
+
+            }
+            else
+            {
+                MessageBox.Show("Senha incorreta!");
+            }
+        }
+
+        private void novoForm()
+        {
+            Application.Run(new Form2());
+        }
+    }
+}
